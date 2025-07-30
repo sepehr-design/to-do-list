a1816131b5577d8d880c9217bf565a9150c2f90c
sepehr-design <sepehr.asemani80@gmail.com>
2025-07-30 18:03:34 +0330

    connecting change mode button and bug fix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let subBox;
 function listGen(act,bool) {
     bool = bool.toString();
     let mainDiv= document.createElement("div");
-    mainDiv.classList.add('listIt');
+    mainDiv.classList.add('listIt','darkModeListIt');
     let mainP = document.createElement("p");
     mainP.classList.add('pList');
     let mainText = document.createTextNode(act);
@@ -14,19 +14,19 @@ function listGen(act,bool) {
     listInputDiv.classList.add('listInput');
     let editButton= document.createElement("button");
     let editIcon= document.createElement("i");
-    editIcon.classList.add("fa-edit" ,"icon" ,"EDIcon" ,"SIcon");
+    editIcon.classList.add("fa-edit" ,"icon" ,"EDIcon" ,"SIcon","darkModeSIcon","gIcon");
     editIcon.title = "Edit";
     editButton.appendChild(editIcon)
     editButton.addEventListener("click", editAdd)
     let deleteButton= document.createElement("button");
     let deleteIcon= document.createElement("i");
-    deleteIcon.classList.add("fa-trash" ,"icon" ,"DLIcon" ,"SIcon");
+    deleteIcon.classList.add("fa-trash" ,"icon" ,"DLIcon" ,"SIcon","darkModeSIcon","gIcon");
     deleteIcon.title = "Delete";
     deleteButton.appendChild(deleteIcon);
     deleteButton.addEventListener("click", deleteAdd);
     let checkButton= document.createElement("button");
     let checkIcon= document.createElement("i");
-    checkIcon.classList.add("icon", "SIcon");
+    checkIcon.classList.add("icon", "SIcon", "darkModeSIcon", "gIcon");
     if(bool=="true"){
         checkIcon.classList.add("fa-check-circle-o" ,"CheckIconTrue");
     }else {
@@ -68,10 +68,12 @@ function check() {
 }
 function openAdd(){
     let hoverBox = document.getElementById("hoverTab");
+    document.getElementById('darkModeAdded').innerHTML = "Add Your Task";
     hoverBox.style.display = "flex";
 }
 function editAdd(){
     openAdd();
+    document.getElementById('darkModeAdded').innerHTML = "Edit Your Task";
     let editBox = this.parentElement;
     let checkingBe = editBox.children[2];
     checking = checkingBe.children[0];
@@ -86,6 +88,49 @@ function deleteAdd(){
         mainDelete.remove();
     }else {}
 }
+function changeMode(){
+    let change = document.getElementById("DarkToggle")
+    if(change.classList.contains('fa-toggle-off')){
+        change.classList.replace('fa-toggle-off','fa-toggle-on');
+        changeStyle();
+    }else{
+        change.classList.replace('fa-toggle-on','fa-toggle-off');
+        changeStyle();
+    }
+}
+function changeStyle(){
+    const body = document.body;
+    body.classList.toggle('darkModeBody');
+    const main = document.getElementById("darkModeMain");
+    main.classList.toggle('darkModeMain');
+    const header = document.getElementById("darkModeHeader");
+    header.classList.toggle('darkModeHeader');
+    const add = document.getElementById("add");
+    add.classList.toggle('darkModeAdd');
+    const mode = document.getElementById("darkModeMode");
+    mode.classList.toggle('darkModeMode');
+    const modeButtonT = document.getElementById("DarkToggle");
+    modeButtonT.classList.toggle('darkModeMode-buttonT');
+    const listCon = document.getElementById("fatherlist");
+    listCon.classList.toggle('darkModeListCon');
+    const SIcon = document.getElementById("darkSIcon");
+    SIcon.classList.toggle('darkModeSIcon');
+    SIcon.classList.toggle('darkModeUper');
+    const textEditor = document.getElementById("darkModeTextEditor");
+    textEditor.classList.toggle('darkModeTextEditor');
+    const added = document.getElementById("darkModeAdded");
+    added.classList.toggle('darkModeAdded');
+    const text = document.getElementById("text");
+    text.classList.toggle('darkModeText');
+    const SIcon2 = document.getElementById("darkSIcon2");
+    SIcon2.classList.toggle('darkModeSIcon');
+    let list2 = document.getElementsByClassName("gIcon")
+    for(i in list2){
+        list2[i].classList.toggle('darkModeSIcon');
+    }
+}
+let changer = document.getElementById("darkMode");
+changer.addEventListener("click", changeMode)
 let page = document.getElementById("add");
 page.addEventListener('click',openAdd)
 function closeAdd(){
